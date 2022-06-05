1ed585198a9199d390f2d59548a390ed2979884c
Kable24 <rifatdavids2401@gmail.com>
2022-06-05 13:34:10 -0700

    hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
   */
 
 int main() {
-  char str1 [] = "Programming is like building a multilingual puzzle"
+  char str1 [] = "Programming is like building a multilingual puzzle";
 
     puts(str1);
 
