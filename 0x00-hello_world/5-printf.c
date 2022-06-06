e805f681d71e750c96cf96d4258a90b1e91c76d7
Kable24 <rifatdavids2401@gmail.com>
2022-06-05 21:23:06 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
  * Hello-print(f)
- * printf - Prints a string using printf
+ * main - Prints a string using printf
  * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, ");
+printf"with proper grammar, but the outcome is a piece of art, "
+  ;
 
 return (0);
 }
