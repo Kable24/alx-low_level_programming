91f8cdddad5e979ad8f8cb157076bb36b93f410e
Kable24 <rifatdavids2401@gmail.com>
2022-06-06 05:46:39 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *Hello-Put
- *main -
- *Return: 0 (Success)
+ *main - puts a string of characters
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
 char str1[] = "\"Programming is like building a multilingual puzzle";
 /**
- *'str1' - This puts "Programming is like building a multilingual puzzle"
+ *str1: This puts "Programming is like building a multilingual puzzle"
  */
 puts(str1);
 
