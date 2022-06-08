967dfca02277da6a4244fea9d934811a235d9c3c
Kable24 <rifatdavids2401@gmail.com>
2022-06-08 09:07:16 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - prints the alphabet
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+
+	return (0);
+}
