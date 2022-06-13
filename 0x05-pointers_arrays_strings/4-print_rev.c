f14dc9f6e411a7a13e6f5e08261b360e86dd88f4
Kable24 <105009974+Kable24@users.noreply.github.com>
2022-06-13 20:57:52 +0200

    kable

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
-  * print_rev - prints string backwards
-  * @s: string
-  * Return: 0
-  */
+ * print_rev - prints string backwards
+ * @s: string
+ * Return: void
+ */
 
 void print_rev(char *s)
 {
-int i = 0;
+	int i = 0;
 
-while (s[i] !='\0')
-{
-i++;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
 }
-while (i>= 0)
-{
-_putchar(s[i]);
-i--;
-}
-_putchar('\n');
-}
-
