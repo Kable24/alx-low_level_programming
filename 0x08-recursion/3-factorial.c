fbf72044b8cafc29c6210bf7b5a8398715fb9d2a
Kable24 <105009974+Kable24@users.noreply.github.com>
2022-06-23 16:46:47 +0200

    recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-* factorial - factorial n
-* @n: interger
-* Return: fecursion
-*/
+ * factorial - factorial n
+ * @n: integer arams
+ * Return: recursion
+ */
 
 int factorial(int n)
 {
 	if (n < 0)
 	{
-		return (-1)
+		return (-1);
 	}
 	if (n == 1)
 	{
@@ -18,4 +18,3 @@ int factorial(int n)
 	}
 	return (n * factorial(n - 1));
 }
-
