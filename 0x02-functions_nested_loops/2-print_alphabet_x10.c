542667f6840b2dd832a3d497deea36e1d0ce4007
Kable24 <rifatdavids2401@gmail.com>
2022-06-29 11:29:15 -0700

    ka

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_alphabet - prints the alphabet
+ * Return: 0 always (success)
+ */
+void print_alphabet_x10(void)
+{
+	char ch = 'a';
+int i
+	for (ch = 'a'; ch <= 'z'; ch++)(i=1;i<=10;i++)
+		_putchar(ch);
+
+	_putchar('\n');
+
+}
---
+++ 0x05-poointers_arrays_strings/0-rest_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - upates the value of a pointer
+  *@n: pointer
+  *Return: 0 (always)
+  */
+
+void reset_to_98(int *n)
+{
+*n = 98
+}
+
